3ad9d60b8085473792838c6f0f0227ebe724dae4
yfern328 <yfernan2@u.rochester.edu>
2017-08-08 18:45:06 -0400

    Done.

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -1,1 +1,27 @@
-// Bomb Component Code Goes Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Bomb extends React.Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+
+  render(){
+    return(
+      <div className="bomb">
+        {this.state.secondsLeft === 0 ? <p>Boom!</p> : <p>{this.state.secondsLeft} seconds left before I go boom!</p>}
+      </div>
+    );
+  }
+};
+
+Bomb.propTypes = {
+  initialCount: PropTypes.number
+}
+
+export default Bomb;
--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,1 +1,22 @@
-// Bomb Component Code Goes Here
+import React from 'react';
+
+class ImageSlider extends React.Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+
+  render(){
+    return(
+      <div className="image-slider">
+        I am on slide {this.state.currentSlideIndex}
+      </div>
+    );
+  }
+};
+
+export default ImageSlider;
